fb1026459f243d2bda6ffa373c63867e31e83b05
omarlinp <omarlinp@gmail.com>
2026-03-17 17:16:28 -0400

    Updated the ID scheme to separate the ids from the local storage and the phrases.json file.

--- javascript/storage.js
+++ javascript/storage.js
@@ -1,5 +1,6 @@
 export function getUserPhrases() {
     const phrases = JSON.parse(localStorage.getItem("userPhrases")) || [];
+    console.log(phrases);
     return phrases;
 }
 
@@ -8,7 +9,7 @@ export function addUserPhrase(phrase) {
     const phrases = getUserPhrases();
 
     
-    const nextId = phrases.length ? Math.max(...phrases.map(p => p.id)) + 1 : 1;
+    const nextId = phrases.length ? Math.max(...phrases.map(p => p.id)) + 1 : 10000;
     phrase.id = nextId;
 
     phrases.push(phrase);
